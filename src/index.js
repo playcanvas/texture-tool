e243249c05484a338269e26285dec5bf877321f5
Donovan Hutchence <slimbuck7@gmail.com>
2022-06-28 19:58:29 +0100

    remove other bits from url

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ rightContainer.append(inspectorPanel);
     const loadUrls = (urlParams.load || []).concat(urlParams.assetUrl || []);
     if (loadUrls.length > 0) {
         loadUrls.forEach((url, index) => {
-            textureManager.addTextureByUrl(url, path.getBasename(url), (err, texture) => {
+            textureManager.addTextureByUrl(url, path.getBasename(url).split('?')[0], (err, texture) => {
                 if (!err && texture) {
                     textureManager.selectTexture(texture);
                 }
