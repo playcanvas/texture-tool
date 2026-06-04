e247eddbeedfd1580fb4ab745ee9436182fe8088
kpal <kpal81xd@gmail.com>
2026-06-04 15:36:29 +0100

    Migrate to @playcanvas/eslint-config v3 (/typescript)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,26 @@
-import playcanvasConfig from '@playcanvas/eslint-config';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import typescriptConfig from '@playcanvas/eslint-config/typescript';
 import globals from 'globals';
 
 export default [
-    ...playcanvasConfig,
+    ...typescriptConfig,
     {
         files: ['**/*.ts'],
         languageOptions: {
-            parser: tsParser,
             globals: {
                 ...globals.browser
             }
         },
-        plugins: {
-            '@typescript-eslint': tsPlugin
-        },
-        settings: {
-            'import/resolver': {
-                typescript: {}
-            }
-        },
         rules: {
-            ...tsPlugin.configs.recommended.rules,
             '@typescript-eslint/ban-ts-comment': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             '@typescript-eslint/no-empty-object-type': 'off',
-            'import/no-unresolved': 'off',
+            'import-x/no-unresolved': 'off',
             'jsdoc/require-param-type': 'off',
             'jsdoc/require-returns-type': 'off',
             'lines-between-class-members': 'off',
             'no-duplicate-imports': 'off',
-            'import/order': 'off'
+            'import-x/order': 'off'
         }
     },
     {
@@ -49,7 +37,7 @@ export default [
             }
         },
         rules: {
-            'import/no-unresolved': 'off'
+            'import-x/no-unresolved': 'off'
         }
     },
     {
